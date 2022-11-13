0ba6697f2a5e74f41b6f8d09e1f476f1dc52441a
CuteNyami <cutenyamiyt@gmail.com>
2022-11-13 14:24:02 +0100

    created the PlayerListener.java class

---
+++ src/main/java/net/luconia/spleef/listeners/PlayerListener.java
@@ -0,0 +1,14 @@
+package net.luconia.spleef.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+    }
+
+}
